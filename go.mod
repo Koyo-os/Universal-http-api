54b1805a82417c754695e70612e72e7b484a045e
osamikoyo <v.pylayev@mail.ru>
2025-03-04 05:44:59 +0500

    add config

---
+++ go.mod
@@ -0,0 +1,8 @@
+module github.com/koyo-os/universal-http-api
+
+go 1.23.0
+
+require (
+	github.com/BurntSushi/toml v1.4.0 // indirect
+	github.com/pelletier/go-toml/v2 v2.2.3 // indirect
+)
--- internal/config/config.go
+++ internal/config/config.go
@@ -1,12 +1,36 @@
 package config
 
+import (
+	"io"
+	"os"
+
+	"github.com/pelletier/go-toml/v2"
+)
+
 type Url struct{
     ServiceAddr string
     UrlPrefix string
 }
 
 type Config struct{
-    port string
-    host string
+    Port string
+    Host string
     Urls []Url
+}
+
+func New(path string) (*Config, error) {
+    var cfg Config
+    
+    file, err := os.Open(path)
+    if err != nil{
+        return nil, err
+    }
+
+    body, err := io.ReadAll(file)
+    if err != nil{
+        return nil, err
+    }
+
+    err = toml.Unmarshal(body, &cfg)
+    return &cfg, err
 }
