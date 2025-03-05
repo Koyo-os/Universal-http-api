2b0ed7c855294025f00118e13eda8d8cfda309ca
osamikoyo <v.pylayev@mail.ru>
2025-03-05 12:32:04 +0500

    add handler

--- internal/app/app.go
+++ internal/app/app.go
@@ -1,1 +1,21 @@
 package app
+
+import (
+	"context"
+
+	"github.com/koyo-os/universal-http-api/internal/config"
+	"github.com/koyo-os/universal-http-api/internal/server"
+	"github.com/koyo-os/universal-http-api/pkg/loger"
+)
+
+func App(ctx context.Context) error {
+	logger := loger.New()
+	cfg, err := config.New("config.toml")
+	if err != nil{
+		logger.Error().Err(err)
+		return err
+	}
+
+	s := server.New(cfg)
+	
+}
---
+++ internal/handler/handler.go
@@ -0,0 +1,37 @@
+package handler
+
+import (
+	"net/http"
+	"net/http/httputil"
+	"net/url"
+	"strings"
+
+	"github.com/koyo-os/universal-http-api/internal/config"
+	"github.com/koyo-os/universal-http-api/pkg/loger"
+)
+
+type Handler struct{
+	logger loger.Logger
+	config *config.Config
+}
+
+func (h *Handler) createReverseProxy(target string) *httputil.ReverseProxy {
+	targetURL, err := url.Parse(target)
+	if err != nil {
+		h.logger.Error().Err(err)
+		return nil
+	}
+	return httputil.NewSingleHostReverseProxy(targetURL)
+}
+
+func (h *Handler) MainHandler(w http.ResponseWriter, r *http.Request) {
+	path := r.URL.Path
+	path = strings.Split(path, "/")[0]
+
+	for _, v := range h.config.Urls {
+		if path == v.UrlPrefix {
+			proxy := h.createReverseProxy(v.ServiceAddr)
+			proxy.ServeHTTP(w, r)
+		}
+	}
+}
